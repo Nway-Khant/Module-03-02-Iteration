c279ad31d7f0476d4f5ab98f6241802831257e4b
nway khant <minkhantnway@gmail.com>
2024-05-20 21:08:31 +0630

    adding Iteration

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var Students = ["Kyawgyi", "Zawgyi", "Htawgyi", "Mawgyi" ];
+console.log(Students.length);
+
+for (var i = 0; i < Students.length; i++){
+    console.log("Hay " + Students[i] + " !")
+} 
